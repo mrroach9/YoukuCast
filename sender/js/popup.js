38f88ff8bca8cddf21531bb00645d8dc80aa607c
mrroach9 <zhangwenqi1988@gmail.com>
2014-01-07 09:56:50 -0800

    Finished popup page UI.

--- sender/js/popup.js
+++ sender/js/popup.js
@@ -52,13 +52,92 @@ getVideoList = function(callback) {
     );
 };
 
+convertTimeToString = function(sec) {
+    if (sec < 0) {
+        return null;
+    }
+    var h = Math.floor(sec / 3600);
+    var m = Math.floor(sec / 60) % 60;
+    var s = Math.floor(sec) % 60;
+    hstr = (h == 0) ? '' : ((h < 10 ? '0' : '') + h.toString() + ':');
+    mstr = ((m < 10) ? '0' : '') + m.toString() + ':';
+    sstr = ((s < 10) ? '0' : '') + s.toString();
+    return hstr + mstr + sstr;
+};
+
+refreshVideoTab = function(node, videoInfo) {
+    if (!videoInfo || !node) {
+        return null;
+    }
+    var title = videoInfo['title'] || '无标题';
+    if (title.length > 13) {
+        title = title.substring(0, 13);
+    }
+
+    var link = videoInfo['link'] || '';
+    var thumbnail = videoInfo['thumbnail'] || '../img/default_thumb.jpg';
+
+    var duration = parseInt(videoInfo['duration']) || 0;
+    duration = convertTimeToString(duration);
+
+    var source = videoInfo['source-tab'] || '未知网页';
+    if (source.length > 8) {
+        source = source.substring(0, 8) + '...';
+    }
+    source = '来源：' + source;
+
+    var description = videoInfo['description'] || '暂无介绍';
+    if (description.length > 45) {
+        description = description.substring(0, 45) + '...';
+    }
+
+    node.find('.thumb-img').attr('style', 
+                                 'background-image: url(\'' + thumbnail + '\')');
+    node.find('.title').append(title);
+    node.find('.description').append(description);
+    node.find('.duration').append(duration);
+    node.find('.source').append(source);
+
+    return node;
+};
+
+genVideoTab = function(videoInfo) {
+    var thumbNode = $('<div>').addClass('thumb').append(
+                        $('<span>').addClass('thumb-img'));
+    var infoNode = $('<div>').addClass('info');
+    var titleNode = $('<span>').addClass('title');
+    var descNode = $('<span>').addClass('description');
+    var duraNode = $('<span>').addClass('duration');
+    var sourceNode = $('<span>').addClass('source');
+    // TODO: origin website (e.g. Youku, Tudou, etc.)
+
+    var topDiv = $('<div>').addClass('video-top')
+                           .append(titleNode).append(descNode);
+    var botDiv = $('<div>').addClass('video-bottom')
+                           .append(sourceNode).append(duraNode);
+    infoNode.append(topDiv).append(botDiv);
+
+    var container = $('<div>').addClass('video').append(
+                        $('<a>').attr('href', 'javascript:void(0)')
+                                .append(thumbNode).append(infoNode));
+    return refreshVideoTab(container, videoInfo);
+};
+
+onUpdateVideoTab = function() {
+    var api = $('.scrollable').data('scrollable');
+    $('.page-text').text((api.getIndex() + 1) + '/' + api.getSize());
+}
+
 $(document).ready(function() {
-    $('#log-info').empty();
+    $('.scrollable > .items').empty();
+    $('.scrollable').scrollable({
+        onSeek: onUpdateVideoTab,
+        onAddItem: onUpdateVideoTab
+    });
     getVideoList(function(videoList) {
-        console.log('[Youku Cast] In Callback');
         console.log('[Youku Cast] ' + videoList);
         for (var vid in videoList) {
-            $('#log-info').append('<p>' + videoList[vid] + '</p>');
+            $('.scrollable').data('scrollable').addItem(genVideoTab({}));
         }
     });
 });
